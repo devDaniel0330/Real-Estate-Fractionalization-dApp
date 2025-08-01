e7cec43f0a428f4c3168d0f3c66aa614bd6fd055
devDaniel0330 <danthecoder77@gmail.com>
2025-08-01 17:00:25 +0800

    added admin auth side

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
 import Dashboard from './pages/Dashboard';
+import AdminDashboard from './pages/AdminDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/auth" element={<AuthPage />} />
         <Route path="/" element={<Navigate to="/auth" />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/auth" element={<AuthPage />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute type="user">
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute type="admin">
+              <AdminDashboard />
+            </ProtectedRoute>
+          } />
       </Routes>
     </Router>
   );
