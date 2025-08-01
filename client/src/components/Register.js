b2a1863c3d0eba2c125084e53ecd6d71063b1e1d
devDaniel0330 <danthecoder77@gmail.com>
2025-08-02 00:11:46 +0800

    updated

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const Register = () => {
   const [form, setForm] = useState({
+    username: '',
     address: '',
     password: '',
     isAdmin: false,
@@ -22,6 +23,7 @@ const Register = () => {
 
     try {
       const res = await axios.post('http://localhost:5000/api/register', {
+        username: form.username,
         walletAddress: form.address,
         password: form.password,
         isAdmin: form.isAdmin,
@@ -36,27 +38,11 @@ const Register = () => {
   return (
     <form onSubmit={handleRegister}>
       <h2>{form.isAdmin ? 'Admin Register' : 'User Register'}</h2>
-      <input
-        type="text"
-        name="address"
-        placeholder="Wallet Address"
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        onChange={handleChange}
-        required
-      />
+      <input type="text" name="username" placeholder="Username" value={form.username} onChange={handleChange} required />
+      <input type="text" name="address" placeholder="Wallet Address" onChange={handleChange} required />
+      <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
       <label>
-        <input
-          type="checkbox"
-          name="isAdmin"
-          checked={form.isAdmin}
-          onChange={handleChange}
-        />{' '}
+        <input type="checkbox" name="isAdmin" checked={form.isAdmin} onChange={handleChange} />{' '}
         Register as Admin
       </label>
       <button type="submit">Register</button>
