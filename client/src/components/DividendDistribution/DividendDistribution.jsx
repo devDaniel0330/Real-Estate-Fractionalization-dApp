1d833aa91db4fc9cfd9f874272663922e98fd6a3
Zhe1224 <wellwhat8@gmail.com>
2025-08-18 22:18:50 +0800

    draft web ui

---
+++ client/src/components/DividendDistribution/DividendDistribution.jsx
@@ -0,0 +1,19 @@
+function DividendDistribution(){
+
+    return <>
+    <h1></h1>
+        <>
+        Distributing tokens to 
+        </>
+        <>//Horizontal
+            <>
+                <PropertyPreview></PropertyPreview>
+                <>bought: xx tokens 45%</>
+            </>
+            <button>Change</button>
+        </>
+        <form>
+            
+        </form>
+    </>;
+}
